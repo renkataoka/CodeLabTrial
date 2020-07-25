dbf039c0894d3940fba7b4fae18eeb5d39f47806
renkataoka <530lotus@gmail.com>
2020-07-25 18:24:26 +0900

    launchSecondActivityメソッドにstartActivity()を追加。

--- app/src/main/java/com/example/renkataoka/codelabTrial_ch2/MainActivity.java
+++ app/src/main/java/com/example/renkataoka/codelabTrial_ch2/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.renkataoka.codelabTrial_ch2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-private static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +18,7 @@ private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     public void launchSecondActivity(View view) {
         Log.d(LOG_TAG, "Button clicked!");
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivity(intent);
     }
 }
