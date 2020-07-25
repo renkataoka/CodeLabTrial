18442ac7b580d59e41ed97f88c144afeac84ecfe
renkataoka <530lotus@gmail.com>
2020-07-25 14:51:58 +0900

    launchSecondActivityメソッドを追加。

--- app/src/main/java/com/example/renkataoka/codelabTrial_ch2/MainActivity.java
+++ app/src/main/java/com/example/renkataoka/codelabTrial_ch2/MainActivity.java
@@ -3,12 +3,18 @@ package com.example.renkataoka.codelabTrial_ch2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+private static final String LOG_TAG = MainActivity.class.getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
     }
 }
