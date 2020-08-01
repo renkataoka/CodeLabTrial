8ebd5dfe837b7483639da5b1027a0283b615c277
renkataoka <530lotus@gmail.com>
2020-08-01 16:15:35 +0900

    openWebsiteメソッドを実装。

--- app/src/main/java/com/example/renkataoka/codelabTrial_ch2/MainActivity.java
+++ app/src/main/java/com/example/renkataoka/codelabTrial_ch2/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.renkataoka.codelabTrial_ch2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mWebsiteEdittext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mWebsiteEdittext = findViewById(R.id.website_edittext);
+    }
+
+    public void openWebsite(View view) {
+        String url = mWebsiteEdittext.getText().toString();
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        //インテントを受け取れるアクティビティが存在するかチェックする
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this!");
+        }
     }
 }
