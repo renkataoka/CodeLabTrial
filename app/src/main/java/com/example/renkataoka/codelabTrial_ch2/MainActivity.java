c474845011d9edcc875cd1b6609fd508b7a4e6d5
renkataoka <530lotus@gmail.com>
2020-08-01 16:25:26 +0900

    openLocationメソッドを実装。

--- app/src/main/java/com/example/renkataoka/codelabTrial_ch2/MainActivity.java
+++ app/src/main/java/com/example/renkataoka/codelabTrial_ch2/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.renkataoka.codelabTrial_ch2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.InetAddresses;
 import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,12 +12,14 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private EditText mWebsiteEdittext;
+    private EditText mLocationEdittext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mWebsiteEdittext = findViewById(R.id.website_edittext);
+        mLocationEdittext = findViewById(R.id.location_edittext);
     }
 
     public void openWebsite(View view) {
@@ -24,6 +27,18 @@ public class MainActivity extends AppCompatActivity {
         Uri webpage = Uri.parse(url);
         Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
         //インテントを受け取れるアクティビティが存在するかチェックする
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents", "Can't handle this!");
+        }
+    }
+
+    public void openLocation(View view) {
+        String loc = mLocationEdittext.getText().toString();
+        Uri addressUri = Uri.parse("geo:0,0?q=" + loc);
+        Intent intent = new Intent(Intent.ACTION_VIEW, addressUri);
+
         if (intent.resolveActivity(getPackageManager()) != null) {
             startActivity(intent);
         } else {
