1ff034880e0e0fec4032cda4ba22eba774d86d8b
renkataoka <530lotus@gmail.com>
2020-09-06 11:56:31 +0900

    各イメージにクリックイベントを追加。

--- sec4/app/src/main/java/com/example/renkataoka/droidcafe/MainActivity.java
+++ sec4/app/src/main/java/com/example/renkataoka/droidcafe/MainActivity.java
@@ -57,4 +57,31 @@ public class MainActivity extends AppCompatActivity {
     public void displayToast(String message) {
         Toast.makeText(getApplicationContext(), message, Toast.LENGTH_SHORT).show();
     }
+
+    /**
+     * Shows a message that the donut image was clicked.
+     *
+     * @param view
+     */
+    public void showDonutOrder(View view) {
+        displayToast(getString(R.string.donut_order_message));
+    }
+
+    /**
+     * Shows a message that the ice cream sandwich image was clicked.
+     *
+     * @param view
+     */
+    public void showIceCreamOrder(View view) {
+        displayToast(getString(R.string.ice_cream_order_message));
+    }
+
+    /**
+     * Shows a message that the froyo image was clicked.
+     *
+     * @param view
+     */
+    public void showFroyoOrder(View view) {
+        displayToast(getString(R.string.froyo_order_message));
+    }
 }
