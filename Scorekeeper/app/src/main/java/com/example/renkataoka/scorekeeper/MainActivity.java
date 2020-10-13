a5a19748befb6df9b9f2a3d54128cbe0ae17a1b6
renkataoka <530lotus@gmail.com>
2020-10-13 22:43:00 +0900

    加点・減点機能を追加。

--- Scorekeeper/app/src/main/java/com/example/renkataoka/scorekeeper/MainActivity.java
+++ Scorekeeper/app/src/main/java/com/example/renkataoka/scorekeeper/MainActivity.java
@@ -3,12 +3,65 @@ package com.example.renkataoka.scorekeeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private int mScore1;
+    private int mScore2;
+
+    private TextView mScoreText1;
+    private TextView mScoreText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Find the TextView by ID.
+        mScoreText1 = findViewById(R.id.score_1);
+        mScoreText2 = findViewById(R.id.score_2);
+    }
+
+    /**
+     * Method that handles the onClick of both the decrement buttons
+     *
+     * @param view The button view that was clicked.
+     */
+    public void decreaseScore(View view) {
+        //Get the ID of the button ta\hat was clicked.
+        int viewID = view.getId();
+        switch (viewID) {
+            case R.id.decreaseTeam1:
+                mScore1--;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+
+            case R.id.decreaseTeam2:
+                mScore2--;
+                mScoreText2.setText(String.valueOf(mScore2));
+                break;
+        }
+    }
+
+    /**
+     * Method that handles the onClick of both the increment button.
+     *
+     * @param view The button view that was clicked.
+     */
+    public void increaseScore(View view) {
+        //Get the ID of the button ta\hat was clicked.
+        int viewID = view.getId();
+        switch (viewID) {
+            case R.id.increaseTeam1:
+                mScore1++;
+                mScoreText1.setText(String.valueOf(mScore1));
+                break;
+
+            case R.id.increaseTeam2:
+                mScore2++;
+                mScoreText2.setText(String.valueOf(mScore2));
+                break;
+        }
     }
 }
