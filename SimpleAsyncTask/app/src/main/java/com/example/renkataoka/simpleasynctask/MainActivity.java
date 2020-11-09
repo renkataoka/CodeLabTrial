0b3ed5ca3be71e717ab37f4625b71d528a920bdb
renkataoka <530lotus@gmail.com>
2020-11-09 22:55:43 +0900

    finish task

--- SimpleAsyncTask/app/src/main/java/com/example/renkataoka/simpleasynctask/MainActivity.java
+++ SimpleAsyncTask/app/src/main/java/com/example/renkataoka/simpleasynctask/MainActivity.java
@@ -1,18 +1,44 @@
 package com.example.renkataoka.simpleasynctask;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView mTextView;
+    private static final String TEXT_STATE = "currentText";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Initialize the TextView.
+        mTextView = findViewById(R.id.textView1);
+        //Restore TextView if there is a savedInstanceState.
+        if (savedInstanceState != null) {
+            mTextView.setText(savedInstanceState.getString(TEXT_STATE));
+        }
     }
 
     public void startTask(View view) {
+        mTextView.setText(R.string.napping);
+
+        //Start the AsyncTask.
+        new SimpleAsyncTask(mTextView).execute();
+    }
+
+    /**
+     * TextViewの内容を保持する。
+     *
+     * @param outState
+     */
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putString(TEXT_STATE, mTextView.getText().toString());
     }
 }
