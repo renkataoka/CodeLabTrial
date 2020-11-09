8bc367d6cc4d7d026bd092ed0f5237dee44ee60c
renkataoka <530lotus@gmail.com>
2020-11-10 01:25:28 +0900

    Parse the JSON string

--- WhoWroteIt/app/src/main/java/com/example/renkataoka/whowroteit/FetchBook.java
+++ WhoWroteIt/app/src/main/java/com/example/renkataoka/whowroteit/FetchBook.java
@@ -3,9 +3,11 @@ package com.example.renkataoka.whowroteit;
 import android.os.AsyncTask;
 import android.widget.TextView;
 
-import java.lang.ref.WeakReference;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-import static android.net.wifi.WifiConfiguration.Status.strings;
+import java.lang.ref.WeakReference;
 
 public class FetchBook extends AsyncTask<String, Void, String> {
     private WeakReference<TextView> mTitleText;
@@ -17,12 +19,52 @@ public class FetchBook extends AsyncTask<String, Void, String> {
     }
 
     @Override
-    protected String doInBackground(String... string) {
+    protected String doInBackground(String... strings) {
         return NetworkUtils.getBookInfo(strings[0]);
     }
 
     @Override
     protected void onPostExecute(String s) {
         super.onPostExecute(s);
+        try {
+            // Obtain the JSON array of items from the result string.
+            JSONObject jsonObject = new JSONObject(s);
+            JSONArray itemsArray = jsonObject.getJSONArray("items");
+
+            // Initialize the variables used for the parsing loop.
+            int i = 0;
+            String title = null;
+            String authors = null;
+
+            //Iterate through the itemsArray, checking each book for title and author information.
+            while (i < itemsArray.length() && (authors == null && title == null)) {
+                // Get the current item information.
+                JSONObject book = itemsArray.getJSONObject(i);
+                JSONObject volumeInfo = book.getJSONObject("volumeInfo");
+
+                // Try to get the author and title from the current item,
+                // catch if either field is empty and move on.
+                try {
+                    title = volumeInfo.getString("title");
+                    authors = volumeInfo.getString("authors");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                i++;
+            }
+            if (title != null && authors != null) {
+                mTitleText.get().setText(title);
+                mAuthorText.get().setText(authors);
+            } else {
+                mTitleText.get().setText(R.string.no_results);
+                mAuthorText.get().setText("");
+            }
+
+        } catch (JSONException e) {
+            // If onPostExecute does not receive a proper JSON string, update the UI to show failed results.
+            mTitleText.get().setText(R.string.no_results);
+            mAuthorText.get().setText("");
+            e.printStackTrace();
+        }
     }
 }
