c007d57c13c0b3e9bee485bbe6a731464717762c
renkataoka <530lotus@gmail.com>
2020-11-09 21:12:33 +0900

    Implement doInBackground()

--- SimpleAsyncTask/app/src/main/java/com/example/renkataoka/simpleasynctask/SimpleAsyncTask.java
+++ SimpleAsyncTask/app/src/main/java/com/example/renkataoka/simpleasynctask/SimpleAsyncTask.java
@@ -4,6 +4,7 @@ import android.os.AsyncTask;
 import android.widget.TextView;
 
 import java.lang.ref.WeakReference;
+import java.util.Random;
 
 public class SimpleAsyncTask extends AsyncTask<Void, Void, String> {
     private WeakReference<TextView> mTextView;
@@ -14,6 +15,22 @@ public class SimpleAsyncTask extends AsyncTask<Void, Void, String> {
 
     @Override
     protected String doInBackground(Void... aVoid) {
-        return null;
+
+        //Generate a random number between 0 and 10.
+        Random rand = new Random();
+        int n = rand.nextInt(11);
+
+        // Make the task take long enough that we have time to rotate the phone while it is running.
+        int s = n * 200;
+
+        // Sleep for the random amount of time.
+        try {
+            Thread.sleep(s);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        //Return a String result.
+        return "Awake at last after sleeping for " + s + " milliseconds!";
     }
 }
